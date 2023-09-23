6eab72d6d72db649cad0edaf5d65ec710f6e429e
Ryan Conklin <empty@email>
2023-09-23 04:36:54 -0400

    deleting more mcreator things

--- src/main/java/com/grouptwosoftworks/progressionplus/init/ProgressionPlusModTabs.java
+++ src/main/java/com/grouptwosoftworks/progressionplus/init/ProgressionPlusModTabs.java
@@ -1,7 +1,3 @@
-
-/*
- *    MCreator note: This file will be REGENERATED on each build.
- */
 package com.grouptwosoftworks.progressionplus.init;
 
 import com.grouptwosoftworks.progressionplus.Constants;
